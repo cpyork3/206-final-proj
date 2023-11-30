fae5e4083772acc14a9b54292d4c7f3ec2c0db87
Conor York <conoryork@CY-Macbook-Pro.local>
2023-11-30 12:29:30 -0500

    Creates empty database

--- create_db.py
+++ create_db.py
@@ -1,2 +1,15 @@
 import sqlite3
 import os
+
+def create_database(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path + "/" + db_name)
+    cur = conn.cursor()
+    return cur, conn
+
+def main():
+    cur, conn = create_database('final_proj.db')
+    print(cur, conn)
+
+if __name__ == "__main__":
+    main()
