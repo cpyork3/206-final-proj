6314f1cf64189424d88a76b84256f3416df75ad3
Jake  Martin <jakemartin@Jakes-Laptop.local>
2023-12-05 10:36:13 -0500

    table created and populated for all player info

--- NHL_api.py
+++ NHL_api.py
@@ -26,3 +26,96 @@ def load_nhljson(url, file_path):
 url = 'https://records.nhl.com/site/api/draft'
 file_path = 'nhlapi.json'
 load_nhljson(url, file_path)
+
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
+
+def create_nhltable(cur, conn):
+    cur.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS nhlplayers (
+        playerId INTEGER PRIMARY KEY, firstName TEXT, lastName TEXT, height INTEGER, weight INTEGER
+        )
+        '''
+
+    )
+    conn.commit()
+
+def add_players(filename, cur, conn):
+        # Load .json file and read job data
+    with open(os.path.abspath(os.path.join(os.path.dirname(__file__), filename))) as f:
+        file_data = f.read()
+
+    try:
+        players = json.loads(file_data)
+        for data in players['data']:
+            player_id = data['playerId']
+            first_name = data['firstName']
+            last_name = data['lastName']
+            height = data['height']
+            weight = data['weight']
+
+            cur.execute(
+                """
+                INSERT OR IGNORE 
+                INTO nhlplayers (playerId, firstName, lastName, height, weight)
+                VALUES (?, ?, ?, ?, ?)
+                """,
+                (player_id, first_name, last_name, height, weight)
+            )
+
+        # Commit the changes after the loop
+        conn.commit()
+
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON data: {e}")
+
+
+
+
+
+    # #load .json file and read job data
+    # #THIS IS TO READ IN DATA
+    # f = open(os.path.abspath(os.path.join(os.path.dirname(__file__), filename)))
+    # file_data = f.read()
+    # f.close()
+    # # THE REST
+    # players = json.loads(file_data)
+    # for data in players:
+    #     player_id = data['playerId']
+    #     first_name = data['firstName']
+    #     last_name = data['lastName']
+    #     height = data['height']
+    #     weight = data['weight']
+
+    #     cur.execute(
+    #         """
+    #         INSERT OR IGNORE 
+    #         INTO nhlplayers (playerId, firstName, lastName, height, weight)
+    #         VALUES (?, ?, ?, ?, ?)
+    #         """,
+    #         (player_id, first_name, last_name, height, weight)
+    #         )
+
+
+
+def main():
+    # SETUP DATABASE AND TABLE
+    cur, conn = setUpDatabase('nhlplayers.db')
+    create_nhltable(cur, conn)
+
+    add_players("nhlapi.json",cur, conn)
+
+    # job_and_hire_date(cur, conn)
+
+    # wrong_salary = (problematic_salary(cur, conn))
+    # print(wrong_salary)
+
+    # visualization_salary_data(cur,conn)
+
+if __name__ == "__main__":
+    main()
+    unittest.main(verbosity=2)
