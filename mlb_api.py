2ea5ce273fd24a25fa826391ebb9e76e92137487
Conor York <conoryork@CY-Macbook-Pro.local>
2023-12-05 15:19:46 -0500

    all player data added to db

--- mlb_api.py
+++ mlb_api.py
@@ -28,20 +28,58 @@ def create_json(url, json_file):
 
     return
 
-# create players table
-def create_table(conn, cur):
-    pass
+# create 3 tables
+    # playerinfo, playerid, heightid
+def create_tables(conn, cur):
+    # create table with player ids and names
+    cur.execute('''CREATE TABLE IF NOT EXISTS mlb_playerids 
+                (id INTEGER PRIMARY KEY, name TEXT)''')
+    # create table with playerid, positionid, height, weight
+    cur.execute('''CREATE TABLE IF NOT EXISTS mlb_playerinfo 
+                (id INTEGER PRIMARY KEY, pos_id INTEGER, height INTEGER, weight INTEGER)''')
+    # create table with height strings and equal amt in inches
+    # cur.execute('''CREATE TABLE IF NOT EXISTS heightid 
+    #             (id INTEGER PRIMARY KEY, height TEXT, inches INTEGER)''')
+    conn.commit()
+    return
+
+# add values from json to database
+def add_values(conn, cur, file_name):
+    # create list of dicts from json
+    with open(file_name) as file:
+        data = json.load(file)
+        # loop through list, add data to each table
+        for player in data:
+            if player['id'] == 676265:
+                print(player)
+            # extract height in inches from height string
+            temp = re.findall(r'\d+', player['height'])
+            height = int(temp[0])*12 + int(temp[1])
+
+            # add info to playerid table
+            cur.execute('''INSERT OR IGNORE INTO mlb_playerids 
+                        (id, name) 
+                        VALUES (?,?)''',
+                        (player['id'], player['fullName']))
+
+            # add info to playerinfo table
+            cur.execute('''INSERT OR IGNORE INTO mlb_playerinfo 
+                        (id, pos_id, height, weight) 
+                        VALUES (?,?,?,?)''',
+                        (player['id'], player['primaryPosition']['code'], height, player['weight'])) 
+            
+            conn.commit()
+    
+    return
 
 def main():
-    # cur, conn = create_conn('final_proj.db')
-    # print(cur, conn)
+    cur, conn = create_conn('final_proj.db')
     players_url = '''https://statsapi.mlb.com/api/v1/sports/1/players?season=2022'''
-    create_json(players_url, 'mlb_players.json')
-
-    # test if json loads correctly
-    with open('mlb_players.json') as file:
-        test_dict = json.load(file)
-        print(type(test_dict))
+    json_file = 'mlb_players.json'
+    create_json(players_url, json_file)
+    create_tables(conn, cur)
+    add_values(conn, cur, json_file)
+    print('done')
 
 
 if __name__ == "__main__":
