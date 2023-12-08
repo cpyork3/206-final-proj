de32cebf8895b2efb45e4617049a6287e2da8fd7
Nina Creimer-Almeida <ninacreimer-almeida@Ninas-MacBook-Air-10.local>
2023-12-08 14:27:27 -0500

    Created graphs for nba_players table

--- nba_html.py
+++ nba_html.py
@@ -1,51 +1,97 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import sqlite3
+from string import ascii_lowercase
+import matplotlib.pyplot as plt
 
 
+def scrape_nba_players_info(url = 'https://www.basketball-reference.com/players/'):
+    # Create a dictionary for each player
+    nba_players = []
 
-def scrape_nba_players(url):
-    response = requests.get(url)
+    for letter in ascii_lowercase:
+        url = f'https://www.basketball-reference.com/players/{letter}/'
+        response = requests.get(url)
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            player_table = soup.find('table', id= 'players')
 
-        player_table = soup.find('table', class_='players-list')
-        if player_table:
-            nba_players = []
+            if player_table:
+                for row in player_table.find_all('tr')[1:]:
+                    columns = row.find_all(['th','td'])
+                    player_name = columns[0].text.strip()
+                    position = columns[3].text.strip()
+                    height = columns[4].text.strip()
+                    weight = columns[5].text.strip()
 
-            for row in player_table.find_all('tr')[1:]:
-                columns = row.find_all('td')
-                player_name = columns[0].text.strip()
-                position = columns[3].text.strip()
-                height = columns[4].text.strip()
-                weight = columns[5].text.strip()
-
-                player_info = {
+                    player_info = {
                     'player_name': player_name,
                     'position': position,
                     'height': height,
                     'weight': weight
-                }
+                    }
 
-                nba_players.append(player_info)
+                    nba_players.append(player_info)
+                    # print(nba_players)
 
-            nba_players_json = json.dumps(nba_players, indent=2)
+                nba_players_json = json.dumps(nba_players, indent=2)
 
-            output_file = 'nba_players.json'  
+                output_file = 'nba_players.json'
 
-            with open(output_file, 'w') as json_file:
-                json_file.write(nba_players_json)
+                with open(output_file, 'w', encoding='utf-8') as json_file:
+                    json_file.write(nba_players_json)
+
+
+
+def insert_json_into_sql(json_file):
+    conn = sqlite3.connect('final_proj.db')
+    cur = conn.cursor()
+
+    # cur.execute('''DROP TABLE IF EXISTS nba_players;''')
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS nba_players (
+            player_name TEXT,
+            position TEXT,
+            height TEXT,
+            weight TEXT
+        )
+    ''')
+
+    with open(json_file, 'r') as file:
+        nba_players_data = json.load(file)
+
+    cur.execute('SELECT COUNT(player_name) FROM nba_players')
+    num_rows = cur.fetchone()[0]
+
+    if num_rows == None:
+        num_rows = 0
+    print('Number of rows in nba_players: ', num_rows)
+
+    # can stop limiting after 4 runs
+    if num_rows < 100:
+        for player_info in nba_players_data[num_rows:num_rows+25]:
+            cur.execute('''
+                INSERT OR IGNORE INTO nba_players (player_name, position, height, weight)
+                VALUES (?, ?, ?, ?)
+            ''', (player_info['player_name'], player_info['position'], player_info['height'], player_info['weight']))
+
+    else:
+        for player_info in nba_players_data[num_rows:]:
+            cur.execute('''
+                INSERT OR IGNORE INTO nba_players (player_name, position, height, weight)
+                VALUES (?, ?, ?, ?)
+            ''', (player_info['player_name'], player_info['position'], player_info['height'], player_info['weight']))
+
+    conn.commit()
+    conn.close()
+
 
 def main():
-    players_url = f'https://www.nba.com/players'
-    scrape_nba_players(players_url)
-
-    # test if json loads correctly
-    with open('nba_players.json') as file:
-        test_dict = json.load(file)
-        print(type(test_dict))
-
+    scrape_nba_players_info()
+    insert_json_into_sql('nba_players.json')
 
 if __name__ == "__main__":
     main()
---
+++ nba_visuals.py
@@ -0,0 +1,120 @@
+import sqlite3
+import json
+import matplotlib.pyplot as plt
+
+def calculate_avg_height_weight_by_position(db_file):
+    connection = sqlite3.connect(db_file)
+    cursor = connection.cursor()
+
+    # Select all athletes from the table
+    cursor.execute('SELECT * FROM nba_players')
+    players = cursor.fetchall()
+
+    # Create a dictionary to store height and weight for each position
+    position_data = {}
+
+    # Iterate through players and store height and weight for each position
+    for player in players:
+        position = player[1]
+        height = player[2]
+        weight_str = player[3]
+
+        # Check if weight is a valid numeric value
+        if weight_str and weight_str.isdigit():
+            weight = int(weight_str)  # Convert weight to integer
+
+            # Convert height to inches for easier calculations (assuming format like '6-8')
+            height_inches = int(height.split('-')[0]) * 12 + int(height.split('-')[1])
+
+            if position not in position_data:
+                position_data[position] = {'height': [], 'weight': []}
+
+            position_data[position]['height'].append(height_inches)
+            position_data[position]['weight'].append(weight)
+
+    # Calculate average height and weight for each position
+    avg_data = {}
+    for position, data in position_data.items():
+        avg_height = sum(data['height']) / len(data['height'])
+        avg_weight = sum(data['weight']) / len(data['weight'])
+
+        avg_data[position] = {'average_height': avg_height, 'average_weight': avg_weight}
+
+    return avg_data
+
+def write_avg_data_to_file(avg_data, output_file='avg_data.txt'):
+    with open(output_file, 'w') as file:
+        file.write(json.dumps(avg_data, indent=2))
+
+def plot_bar_charts(position_data):
+    # Create bar charts for height and weight
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(14, 6))
+    
+    axes[0].bar(position_data.keys(), [data['average_height'] for data in position_data.values()])
+    axes[0].set_title('Bar Chart of Average Height by Position')
+    axes[0].set_ylabel('Average Height (inches)')
+
+    axes[1].bar(position_data.keys(), [data['average_weight'] for data in position_data.values()])
+    axes[1].set_title('Bar Chart of Average Weight by Position')
+    axes[1].set_ylabel('Average Weight (lbs)')
+
+    plt.tight_layout()
+    plt.show()
+
+def scatter_plot_height_weight_by_position(db_file):
+    connection = sqlite3.connect(db_file)
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM nba_players')
+    players = cursor.fetchall()
+
+    positions = set(player[1] for player in players)  # Get unique positions
+
+    # Dictionary to store data points for each position
+    position_data = {position: {'height': [], 'weight': []} for position in positions}
+
+    for player in players:
+        position = player[1]
+        height = int(player[2].split('-')[0]) * 12 + int(player[2].split('-')[1])  # Convert height to inches
+
+        # Check if weight is a valid integer
+        weight_str = player[3]
+        try:
+            weight = int(weight_str)
+        except ValueError:
+            continue
+
+        position_data[position]['height'].append(height)
+        position_data[position]['weight'].append(weight)
+
+    plt.figure(figsize=(10, 6))
+
+    for position, data in position_data.items():
+        plt.scatter(data['height'], data['weight'], label=position)
+
+    plt.xlabel('Height (inches)')
+    plt.ylabel('Weight (lbs)')
+    plt.title('Scatter Plot of Height and Weight by Position')
+    plt.legend()
+    plt.grid(True)
+    plt.yticks(range(100, 400, 20))
+
+    plt.show()
+
+def main():
+    db_file = 'final_proj.db'
+
+    # Calculate average height and weight by position
+    avg_data = calculate_avg_height_weight_by_position(db_file)
+
+    # Write the calculated data to a text file
+    write_avg_data_to_file(avg_data)
+
+    # Plot bar charts
+    plot_bar_charts(avg_data)
+
+    # Plot scatter plot
+    scatter_plot_height_weight_by_position(db_file)
+    
+if __name__ == "__main__":
+    main()
